813e84ec194e0e6358015d37b18706f41cf0b318
Gevorg Danelyan <g.danelyan@worksolutions.ru>
2024-02-05 14:48:39 +0300

    fix(exports): export middleware interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
-export { RequestManager } from './requestManager';
-export { AxiosRequestEngine } from './requestEngines/axiosRequestEngine';
+export { JsonousDataDecoder } from './decoders/jsonousDataDecoder';
+
 export { DecoderError } from './errors/decoderError';
 export { RequestEngineError } from './errors/requestEngineError';
-export { JsonousDataDecoder } from './decoders/jsonousDataDecoder';
+
 export type { RequestEngine, RequestEngineResponse } from './interfaces/requestEngine';
 export type { DataDecoder } from './interfaces/dataDecoder';
 export type { RequestConfig, Method, ContentType, ResponseType, ResponseEncoding } from './interfaces/requestConfig';
+export type { RequestMiddlewareConfig, ResponseMiddlewareConfig, ErrorMiddlewareConfig } from './interfaces/middleware';
+
+export { AxiosRequestEngine } from './requestEngines/axiosRequestEngine';
+
+export { RequestManager } from './requestManager';
