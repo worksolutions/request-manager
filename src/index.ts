05b21b7d6161ef7d9dd42235581f620a2cd41d86
Gevorg Danelyan <g.danelyan@worksolutions.ru>
2024-02-05 11:53:38 +0300

    fix(exports): export JsonousDataDecoder

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export { RequestManager } from './requestManager';
 export { AxiosRequestEngine } from './requestEngines/axiosRequestEngine';
 export { DecoderError } from './errors/decoderError';
 export { RequestEngineError } from './errors/requestEngineError';
+export { JsonousDataDecoder } from './decoders/jsonousDataDecoder';
 export type { RequestEngine, RequestEngineResponse } from './interfaces/requestEngine';
 export type { DataDecoder } from './interfaces/dataDecoder';
 export type { RequestConfig, Method, ContentType, ResponseType, ResponseEncoding } from './interfaces/requestConfig';
